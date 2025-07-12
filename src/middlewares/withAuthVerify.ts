caf2bc1e1c716de50a8882c77f03b79e97ec32fa
Matteo Tinta <matteo.tinta.external@ynap.com>
2025-07-12 13:35:32 +0200

    chore: var to const

--- src/middlewares/withAuthVerify.ts
+++ src/middlewares/withAuthVerify.ts
@@ -1,29 +1,27 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { JsonWebTokenError } from 'jsonwebtoken';
 import { container } from '../dependiencies';
 import { JwtService } from '../core/services/jwt.service';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  
-    var [, token] = req.headers["authorization"]?.split("Bearer") || []
-    if(!token) {
-        res.status(401).send({ ok: false, reason: "empty authorization" })
-        return;
+  const [, token] = req.headers['authorization']?.split('Bearer') || [];
+  if (!token) {
+    res.status(401).send({ ok: false, reason: 'empty authorization' });
+    return;
+  }
+
+  try {
+    const service = await container.getAsync(JwtService);
+    req.user = service.verifyAndDecode(token);
+  } catch (error) {
+    console.error(error);
+    if (error instanceof JsonWebTokenError) {
+      res.status(401).send({ ok: false, reason: error.message });
+      return;
     }
 
-    
-    try {
-        var service = await container.getAsync(JwtService)
-        req.user = service.verifyAndDecode(token);
-    } catch (error) {
-        console.error(error)
-        if(error instanceof JsonWebTokenError) {
-            res.status(401).send({ ok: false, reason: error.message })
-            return;
-        }
+    throw error;
+  }
 
-        throw error;
-    }
-
-    next()
-}
+  next();
+};
